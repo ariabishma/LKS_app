392252a7b64b629228d38182c329b7bd81b099c2
bishma <ariabishma848@gmail.com>
2019-10-03 20:27:05 +0700

    view

--- views/home.bsm.php
+++ views/home.bsm.php
@@ -1,14 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <?php $this->Component('header',['nama'=>'bishma']); ?>
+
+    <?php $this->Component('header'); ?>
+    
+    
     <h1>HEllo WOrld </h1>
-    <?php print_r($data) ?>
-</body>
-</html>
+
+    
+    <?php
+        foreach ($data as  $value) {
+            echo $value['username']."</br>";
+        }
+    ?>
+
+
+    <?php $this->Component('footer'); ?>
